7a8ce574f2a94fba79f219b4fdc8b337a1cf8c3a
SUTEJREDDYPUDIPARTHI <139585194+SUTEJREDDYPUDIPARTHI@users.noreply.github.com>
2024-11-28 14:15:55 +0530

    Add files via upload

---
+++ Flask Deployed App/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask, redirect, render_template, request
+from PIL import Image
+import torchvision.transforms.functional as TF
+import CNN
+import numpy as np
+import torch
+import pandas as pd
